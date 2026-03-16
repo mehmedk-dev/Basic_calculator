ee240414d6b679109abd60dc4d46bc340afb6609
mehmed06 <crawlihell@gmail.com>
2026-03-16 19:36:50 +0200

    first commit

---
+++ basic_calculator/basic_calculator.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	int choice = -100;
+
+	while (choice !=0)
+	{
+		
+		//Menu
+		cout << "1 - Addition numbers\n";
+		cout << "2 - Subtract numbers\n";
+		cout << "0 - Exit\n";
+		cout << "Choose: \n";
+		cin >> choice;
+		if (choice == 0)
+		{
+			cout << "Exiting program...." << endl;
+			break;
+		}
+		else if (choice == 1 || choice == 2)
+		{
+			cout << "Enter first number: ";
+			cin >> a;
+
+			cout << "Enter second number: ";
+			cin >> b;
+
+			if (choice == 1)
+				cout << "Total is: " << a + b << endl;
+			else
+				cout << "Total is: " << a - b << endl;
+		}
+		else
+		{
+			cout << "Invalid choice\n";
+		}
+
+		cout << "_____________________" << endl;
+	}
+
+
+
+
+
+
+
+	return 0;
+
+
+}
